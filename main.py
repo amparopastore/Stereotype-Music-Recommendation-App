d8bf7b4e180a3a58ad22739619c5bc37289d02a5
Amparo Godoy Pastore <agodoypastore@gmail.com>
2024-03-22 16:39:13 -0400

    new changes

--- main.py
+++ main.py
@@ -2,43 +2,60 @@
 import flask
 from flask import Flask, render_template, redirect, url_for, session, request
 import os
-
+from user import SpotifyClient
+import config
 
 # initialize flask app instance
 app = Flask(__name__)
 
 # configuration settings
-app.secret_key = os.random(2)
+app.secret_key = os.urandom(2)
 app.config['SESSION_COOKIE_NAME'] = "StereoType Cookie"
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+client_id = config.SPOTIFY_CLIENT_ID
+client_secret = config.SPOTIFY_CLIENT_SECRET
+redirect_uri = config.REDIRECT_URI
 
 
 #--------------------------------- LOGIN/HOME ---------------------------------
+spotify_client = SpotifyClient(client_id, client_secret, redirect_uri)
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    """
-    Redirect users to login page
-    """
-
-@app.route("/login", methods=['GET'])
-def login():
     """
     Redirect to Spotify's login page
     """
+    authorization_url = spotify_client.request_authorization_url()
+    return redirect(authorization_url)
 
-@app.route("/callback", methods=['GET'])
+@app.route("/callback/q")
 def callback():
     """
     Handle callback from Spotify after user login
     """
+    # Extract authorization code from query parameters
+    authorization_code = request.args['code']
+    
+    # Exchange authorization code for access token
+    access_token_info = spotify_client.exchange_code_for_token(authorization_code)
 
-@app.route("/home", methods=['GET'])
+    # Store access token in session
+    if access_token_info:
+        session['access_token'] = access_token_info['access_token']
+        return redirect(url_for('home',_external=True))
+    
+    return "Authentication failed."
+
+@app.route("/home")
 def home():
     """
     Render home page after successful login
     """
+    if 'access_token' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('index'))
 
 #-------------------------------- RECOMMENDATIONS ---------------------------------
 # Artist recommendations
@@ -46,3 +63,8 @@ def home():
 # Song recommendations
 
 #--------------------------------- ERROR HANDLER ---------------------------------
+
+
+#--------------------------------- RUN LOCALLY --------------------------------- 
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
--- user.py
+++ user.py
@@ -1,11 +1,57 @@
-class User:
-    def __init__(self) -> None:
-        pass
+import requests
+import base64
 
-    # authentication and authorization 
+class SpotifyClient:
+    AUTHORIZATION_URL = 'https://accounts.spotify.com/authorize?'
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
-    # user profile information
+    #STATE = ''
+    SCOPE = 'user-top-read playlist-read-private'
 
-    # retrieve top tracks and artists
+    def __init__(self, client_id, client_secret, redirect_uri) -> None:
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+        self.access_token = None
 
-    # error handling
+    def request_authorization_url(self):
+        params = {
+            'client_id': self.client_id,
+            'response_type': 'code',
+            'redirect_uri': self.redirect_uri,
+            #'state': self.STATE,
+            'scope': self.SCOPE
+        }
+        return requests.Request('GET', self.AUTHORIZATION_URL, params=params).prepare().url
+    
+    def exchange_code_for_token(self, code):
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(f"{self.client_id}:{self.client_secret}".encode()).decode(),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': self.redirect_uri
+        }
+        response = requests.post(self.TOKEN_URL, headers=headers, data=data)
+        if response.status_code == 200:
+            self.access_token = response.json()['access_token']
+            return response.json()
+        else:
+            # raise exception with error message
+            response.raise_for_status()
+    
+    def get_user_profile(self, access_token):
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        response = requests.get('https://api.spotify.com/v1/me', headers=headers)
+        response.raise_for_status()  # raise exception if response is not 200
+        return response.json()
+
+
+
+    # class UserData(SpotifyClient):
+    #     def __init__(self, client_id, client_secret, redirect_uri, access_token) -> None:
+    #         super().__init__(client_id, client_secret, redirect_uri)
+    #         self.access_token = access_token
+
