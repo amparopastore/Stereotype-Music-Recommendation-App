f71b798ae948cbd270ce0904e3de037ba2520209
Amparo Godoy Pastore <agodoypastore@gmail.com>
2024-04-11 23:26:05 -0400

    recent changes

---
+++ helpers/client.py
@@ -0,0 +1,106 @@
+import requests
+import base64
+import os
+import urllib.parse
+
+class SpotifyClient:
+    AUTHORIZATION_URL = 'https://accounts.spotify.com/authorize'
+    TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    
+    #STATE = ''
+    SCOPE = 'user-top-read playlist-read-private'
+    SHOW_DIALOG = True
+
+    def __init__(self, client_id, client_secret, redirect_uri) -> None:
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+        self.access_token = None
+        self.state = None
+
+    def gen_rand_str(self, length):
+        return os.urandom(length).hex()
+    
+    #  ------------------ Authorization ------------------
+    def request_auth_url(self):
+        self.state = self.gen_rand_str(16)
+        params = {
+            'client_id': self.client_id,
+            'response_type': 'code',
+            'redirect_uri': self.redirect_uri,
+            'scope': self.SCOPE,
+            'show_dialog': str(self.SHOW_DIALOG).lower(),
+            'state': self.state
+        }
+        # return requests.Request('GET', self.AUTHORIZATION_URL, params=params).prepare().url
+        return f"{self.AUTHORIZATION_URL}?{urllib.parse.urlencode(params)}"
+    
+    def exchange_code_for_token(self, code, state):
+        if state != self.state:
+            raise ValueError("State mismatch")
+        headers = {
+            'Authorization': 'Basic ' + base64.b64encode(f"{self.client_id}:{self.client_secret}".encode()).decode(),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': self.redirect_uri
+        }
+        response = requests.post(self.TOKEN_URL, headers=headers, data=data)
+        response.raise_for_status()
+        token_info = response.json()
+        self.access_token = token_info['access_token']
+        return token_info
+
+    def get_user_profile(self):
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        response = requests.get('https://api.spotify.com/v1/me', headers=headers)
+        response.raise_for_status()  # raise exception if response is not 200
+        return response.json()
+
+# ------------------ Recommendations ------------------
+    
+    def get_top_items(self, type='artists'):
+        url = f'https://api.spotify.com/v1/me/top/{type}'
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        response = requests.get(url, headers=headers)
+        return response.json()['items']
+
+    
+    def get_artist_recommendations(self):
+        endpoint_url = 'https://api.spotify.com/v1/artists/{id}/related-artists'
+        top_artists = self.get_top_items(type='artists') 
+        seed_artists = [artist['id'] for artist in top_artists[:5]] 
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        recommendations = []
+        for artist_id in seed_artists:
+            url = endpoint_url.format(id=artist_id)
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                related_artists_data = response.json().get('artists', [])
+                if related_artists_data:  # Check if there are related artists
+                    first_related_artist = related_artists_data[0]  # Fetching data for the first related artist
+                    artist_info = {
+                        'id': first_related_artist['id'],
+                        'name': first_related_artist['name'],
+                        'external_urls': first_related_artist['external_urls']['spotify'],
+                        'popularity': first_related_artist['popularity'],
+                        'followers': first_related_artist['followers'],  # Fetching followers
+                        'images': first_related_artist['images']  # Fetching images
+                    }
+                    recommendations.append(artist_info)
+            else:
+                print(f"Failed to fetch related artists for artist with ID {artist_id}")
+        return recommendations
+
+
+    def get_track_recommendations(self):
+        endpoint_url = 'https://api.spotify.com/v1/recommendations'
+        top_tracks = self.get_top_items(type='tracks')  
+        seed_tracks = [track['id'] for track in top_tracks[:5]]
+        headers = {'Authorization': f'Bearer {self.access_token}'}
+        params = {'seed_tracks': ','.join(seed_tracks)}
+        response = requests.get(endpoint_url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import flask
 from flask import Flask, render_template, redirect, url_for, session, request
 import os
-from user import SpotifyClient
+from helpers.client import SpotifyClient
 import config
 
 # initialize flask app instance
@@ -18,7 +18,7 @@ client_secret = config.SPOTIFY_CLIENT_SECRET
 redirect_uri = config.REDIRECT_URI
 
 
-#--------------------------------- LOGIN/HOME ---------------------------------
+#--------------------------------- LOGIN/HOME PAGE ---------------------------------
 spotify_client = SpotifyClient(client_id, client_secret, redirect_uri)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -27,7 +27,7 @@ def index():
     Redirect to Spotify's login page
     """
     authorization_url = spotify_client.request_auth_url()
-    return redirect(authorization_url)
+    return render_template('index.html', authorization_url=authorization_url)
 
 @app.route("/callback", methods=['GET'])
 def callback():
@@ -50,12 +50,43 @@ def home():
     """
     Render home page after successful login
     """
-    return render_template('index.html')
+    user_profile = spotify_client.get_user_profile()
+    return render_template('index.html', user_profile=user_profile)
 
-#-------------------------------- RECOMMENDATIONS ---------------------------------
-# Artist recommendations
+@app.route("/about",  methods=['GET'])
+def about():
+    return render_template('about.html')
 
-# Song recommendations
+@app.route("/trending",  methods=['GET'])
+def trending():
+    return render_template('trending.html')
+
+#-------------------------------- RECOMMENDATIONS PAGE ---------------------------------
+# Artist & track recommendations
+
+@app.route("/recommend_start", methods=['GET', 'POST'])
+def recommend_start():
+    if request.method == 'GET':
+        return render_template('recommend_start.html')
+    elif request.method == 'POST':
+        seed_type = request.form['seed_type'] 
+        if seed_type == 'artist':
+            return redirect(url_for('recommend_artists'))
+        elif seed_type == 'track':
+            return redirect(url_for('recommend_songs'))
+
+@app.route("/recommend_artists", methods=['GET', 'POST'])
+def recommend_artists():
+    top_artists = spotify_client.get_top_items(type='artists')
+    recommendations = spotify_client.get_artist_recommendations()
+    return(render_template('artists_recs.html', top_artists=top_artists, recommendations=recommendations))
+
+@app.route("/recommend_songs", methods=['GET', 'POST'])
+def recommend_songs():
+    top_songs = spotify_client.get_top_items(type='tracks')
+    recommendations = spotify_client.get_track_recommendations()
+    return(render_template('songs_recs.html', top_songs=top_songs, recommendations=recommendations))
+
 
 #--------------------------------- ERROR HANDLER ---------------------------------
 
